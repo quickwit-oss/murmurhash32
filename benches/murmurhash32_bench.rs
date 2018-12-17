6f5810c2644abd43cf93eb44e85a5cd71d797278
Paul Masurel <paul.masurel@gmail.com>
2018-12-17 10:14:55 +0900

    First commit

---
+++ benches/murmurhash32_bench.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+use murmurhash32::murmurhash2;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("murmurhash2 5", |b| b.iter(|| murmurhash2(b"turtl")));
+    c.bench_function("murmurhash2 10", |b| b.iter(|| murmurhash2(b"liketurtles")));
+    c.bench_function("murmurhash2 15", |b| b.iter(|| murmurhash2(b"i like turtles!")));
+    c.bench_function("murmurhash2 100", |b| b.iter(|| murmurhash2(b"maitre corbeau sur un arbre perche tenait dans son bec un fromage. Maitre renard par l'odeur alleche")));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
